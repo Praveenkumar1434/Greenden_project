3b992c0f4ba65422fffb9d35fefecf20eb67dd44
Praveenkumar1434 <mpraveen1432004@gmail.com>
2025-07-15 15:45:01 +0530

    search

--- product.js
+++ product.js
@@ -8,3 +8,19 @@ menuicon.addEventListener("click",()=>{
 closeicon.addEventListener("click",()=>{
     sidenav.style.right="-50%"
 })
+
+let productContainer=document.getElementById("product__container")
+let search=document.getElementById("search")
+let divlist=productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",()=>{
+    let enteredvalue=event.target.value.toUpperCase()
+    for(count=0; count<divlist.length;count++){
+        let productname=divlist[count].querySelector("h1").textContent
+        if(productname.toUpperCase().indexOf(enteredvalue)<0){
+            divlist[count].style.display="none"
+        }else{
+            divlist[count].style.display="block"
+        }
+    }
+})
